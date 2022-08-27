c5a6baee70463a0713f9d90500093cbce09b21d1
VASHvic <vforvanish@gmail.com>
2022-08-27 12:34:12 +0200

    C loops

---
+++ C/loops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+
+  int i;
+
+  for(int i=0;i<5;i++){
+    if(i <3){
+    printf("%d\n",i);
+    }
+  }
+  return 0;
+}
