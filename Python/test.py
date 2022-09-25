1e731001d89e88e15c679cb31763d7232247ce38
VASHvic <vforvanish@gmail.com>
2022-09-25 11:53:44 +0200

    sql alter tables

---
+++ Python/test.py
@@ -0,0 +1,4 @@
+for x in range(7):
+    if x == 6:
+        print(x)
+print(x)
--- SQL/action.sql
+++ SQL/action.sql
@@ -1,5 +1,10 @@
 
-// video 14
-// consultas de datos anexados(adir tablas externas)
+// consultas de datos anexados(juntar tablas externas)
 INSERT INTO clientes
 SELECT * FROM clientes_madrid
+
+//video 17 modificar tablas
+ALTER TABLE ciudades ADD column nombre VARCHAR(255)  
+ALTER TABLE clientes ALTER COLUMN fecha DATE
+
+
